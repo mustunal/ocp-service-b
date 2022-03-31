adf0f1e57180f88abcdb5fcdcf2c10089ca55422
TKiccteam <iccteam@thy.com>
2022-03-31 22:57:57 +0300

    initial commit

---
+++ src/main/java/com/mustunal/serviceb/ApiController.java
@@ -0,0 +1,17 @@
+package com.mustunal.serviceb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello World From Service-B";
+    }
+}
